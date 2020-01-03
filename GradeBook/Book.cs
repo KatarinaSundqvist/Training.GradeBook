ff2d0bd1737d4f216b66f481b1ee96d2aec42cf6
KatarinaSundqvist <singsongkat@live.com>
2020-01-03 12:43:25 +0100

    Add project files.

---
+++ GradeBook/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook {
+    public class Book {
+
+        public Book(string name) {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade) {
+            grades.Add(grade);
+        }
+
+        public Stats GetStats() {
+            var result = new Stats();
+            var gradeTotal = 0.0;
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            foreach (var grade in grades) {
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                gradeTotal += grade;
+            }
+            result.Average = gradeTotal / grades.Count;
+            return result;
+        }
+
+        private List<double> grades;
+
+        private string name;
+    }
+}
---
+++ GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook {
+    class Program {
+        static void Main(string[] args) {
+
+            var book = new Book("Scott's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            var stats = book.GetStats();
+            Console.WriteLine($"The average of the grades is {stats.Average:N1}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+
+        }
+    }
+}
